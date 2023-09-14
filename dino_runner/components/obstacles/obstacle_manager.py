0c1d48bc95216d48eddb130ed9b86541dfb98e74
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-14 16:35:36 -0300

    feat: remove unused lines

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,6 @@
 import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 class ObstacleManager:
   def __init__(self):
@@ -9,7 +8,7 @@ class ObstacleManager:
 
   def update(self, game):
     if len(self.obstacles) == 0:
-      self.obstacles.append(Cactus([SMALL_CACTUS, LARGE_CACTUS], "random"))
+      self.obstacles.append(Cactus())
 
     for obstacle in self.obstacles:
       obstacle.update(game.game_speed, self.obstacles)
