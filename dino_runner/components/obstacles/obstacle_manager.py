06100d14dd1f7daa178fecf30f1dacdaac6e3989
Claudio Cassimiro <65298393+claudiocassimiro@users.noreply.github.com>
2023-09-16 17:40:18 -0300

    feat: add new constants, method to play music and logic to power_ups

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -3,11 +3,13 @@ import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
+from dino_runner.components.obstacles.explosion import Explosion
+from dino_runner.utils.constants import HAMMER_TYPE, EXPLOSION_SOUND, DEATH_SOUND, DINO_DEAD
+
 
 class ObstacleManager:
-  def __init__(self, player):
+  def __init__(self):
     self.obstacles = []
-    self.player = player
 
   def update(self, game):
     obstacle_type = [
@@ -17,14 +19,24 @@ class ObstacleManager:
 
     if len(self.obstacles) == 0:
       self.obstacles.append(obstacle_type[random.randint(0, 1)])
-
+    
     for obstacle in self.obstacles:
       obstacle.update(game.game_speed, self.obstacles)
-      if game.player.dino_rect.colliderect(obstacle.rect) and self.player.type != "shield":
-        pygame.time.delay(500)
-        game.playing = False
-        game.death_count += 1
-        break
+      if game.player.dino_rect.colliderect(obstacle.rect):
+        if not game.player.has_power_up:
+          game.player.image = DINO_DEAD[0]
+          self.play_sound_effect(DEATH_SOUND)
+          pygame.time.delay(3000)
+          game.playing = False
+          game.death_count += 1
+          break
+        elif game.player.type == HAMMER_TYPE:
+          rect_x = obstacle.rect.x
+          rect_y = obstacle.rect.y
+          explosion = Explosion(rect_x, rect_y)
+          self.play_sound_effect(EXPLOSION_SOUND)
+          self.obstacles.append(explosion)
+          self.obstacles.remove(obstacle)
   
   def reset_obstacles(self):
     self.obstacles = []
@@ -32,3 +44,8 @@ class ObstacleManager:
   def draw(self, screen):
     for obstacle in self.obstacles:
       obstacle.draw(screen)
+  
+  def play_sound_effect(self, sound):
+    pygame.mixer.music.load(sound)
+    pygame.mixer.music.play()
+    pygame.mixer.music.set_volume == 40
